f4e62c7edeb880932425ad6fd56f91aef38f6478
Basix <basix@basix.tech>
2020-04-18 19:55:04 +0900

    feat: capsule

--- src/routes/capsules.ts
+++ src/routes/capsules.ts
@@ -3,13 +3,27 @@ import { getRepository } from "typeorm";
 import { doAsync } from '../utils/doAsync';
 
 import { Capsule } from "../entities/Capsule";
+import { AuthorizedRequest } from "../utils/types";
 
 const router = express.Router();
 
-const repository = getRepository(Capsule);
+type GeographicRequest = AuthorizedRequest<{
+  lat: string, 
+  lng: string 
+}>;
 
-router.get("/", function(req, res, next) {
+router.get("/", doAsync(async function(req: GeographicRequest, res, next) {
+  const repository = getRepository(Capsule);
+  const capsules = await repository.createQueryBuilder('capsule')
+    .select()
+    .where(`POW(111.321 * (lat - :lat), 2) + POW(111.321 * (:lng - lng) * COS(latitude / 57.3), 2) <= 1`) // within 1 km
+    .setParameters({ lat: req.body.lat, lng: req.body.lng })
+    .limit(3)
+    .printSql()
+    .getMany()
+  return res.send({ capsules })
+}));
 
-});
+
 
 export default router;
