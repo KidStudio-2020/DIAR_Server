845a19fac011af1d655fd1b9a34fa14b781255e1
Basix <basix@basix.tech>
2020-04-18 21:38:28 +0900

    feat: implement add capsule

--- src/routes/capsules.ts
+++ src/routes/capsules.ts
@@ -4,6 +4,7 @@ import { doAsync } from '../utils/doAsync';
 
 import { Capsule } from "../entities/Capsule";
 import { AuthorizedRequest } from "../utils/types";
+import { User } from "../entities/User";
 
 const router = express.Router();
 
@@ -24,6 +25,33 @@ router.get("/", doAsync(async function(req: GeographicRequest, res, next) {
   return res.send({ capsules })
 }));
 
+type CapsuleRequest = AuthorizedRequest<{
+  title: string,
+  imagePaths: string[],
+  description: string,
+  placeName: string
+}> & GeographicRequest;
 
+router.post('/', doAsync(async function(req: CapsuleRequest, res, next) {
+  const repository = getRepository(Capsule);
+
+  if(!req.body || !req.body.lat || !req.body.lng || !req.body.title || !req.body.imagePaths || !req.body.description || !req.body.placeName)
+    return res.status(400).json({ error: 'invalid' });
+
+  const user = await getRepository(User).findOne(req.user!.id, { relations: ['user'] });
+  if(!user) throw Error('No user found, but authorized');
+
+  const capsule = new Capsule();
+  
+  capsule.lat = req.body.lat;
+  capsule.lng = req.body.lng;
+  capsule.title = req.body.title;
+  capsule.placeName = req.body.placeName;
+  capsule.description = req.body.description;
+  capsule.imagePaths = req.body.imagePaths;
+  capsule.user.push(user);
+
+  repository.save(capsule);
+}))
 
 export default router;
